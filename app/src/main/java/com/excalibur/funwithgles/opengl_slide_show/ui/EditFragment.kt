87e4c2c33dd704b113f9b8b4c191fc64a84c4f97
Hưởng Vũ <huongvv@solarapp.asia>
2020-12-01 00:52:13 +0700

    Show bitmap canvas

--- app/src/main/java/com/excalibur/funwithgles/opengl_slide_show/ui/EditFragment.kt
+++ app/src/main/java/com/excalibur/funwithgles/opengl_slide_show/ui/EditFragment.kt
@@ -1,8 +1,11 @@
 package com.excalibur.funwithgles.opengl_slide_show.ui
 
+import android.graphics.*
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.Surface
+import android.view.TextureView.SurfaceTextureListener
 import android.view.View
 import android.view.ViewGroup
 import com.excalibur.funwithgles.R
@@ -10,10 +13,11 @@ import com.excalibur.funwithgles.opengl_slide_show.adapter.InfoPathAdapter
 import com.excalibur.funwithgles.opengl_slide_show.base.BaseFragment
 import kotlinx.android.synthetic.main.fragment_edit.*
 
+
 class EditFragment : BaseFragment() {
 
     private lateinit var infoPathAdapter : InfoPathAdapter
-
+    private var l : Surface ? = null
 
 
     override fun onCreateView(
@@ -27,12 +31,54 @@ class EditFragment : BaseFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initLinkAdapter()
+        initTextureView()
+        //updateSizeTexture()
+
     }
 
     private fun initLinkAdapter(){
         infoPathAdapter = InfoPathAdapter()
         rcvList.adapter = infoPathAdapter
         infoPathAdapter.submitList(mainViewModel.listPath.value)
+    }
+
+    //https://stackoverflow.com/questions/29009210/texture-view-video-and-bitmap-display
+    private fun initTextureView(){
+        surfaceView.surfaceTextureListener = surfaceTextureListener
+    }
+
+    private fun updateSizeTexture(){
+        surfaceView.layoutParams.width = 720
+        surfaceView.layoutParams.height = 720
+    }
+
+    private fun showImage(){
+
+    }
+
+    private val surfaceTextureListener = object : SurfaceTextureListener {
+        override fun onSurfaceTextureAvailable(surfaceTexture: SurfaceTexture?, width: Int, height: Int) {
+            l = Surface(surfaceTexture)
+            val bm = BitmapFactory.decodeFile(mainViewModel.listPath.value!![0])
+            val canvas = l?.lockCanvas(Rect(0,0,width,height))
+            canvas?.drawBitmap(bm,(width/2)-(bm.width /2).toFloat(), (height/2)-(bm.height /2).toFloat(), null )
+            l?.unlockCanvasAndPost(canvas)
+            Log.e("HVV1312", "surfaceTextureListener onSurfaceTextureAvailable $width - $height")
+        }
+
+        override fun onSurfaceTextureSizeChanged(p0: SurfaceTexture?, p1: Int, p2: Int) {
+            Log.e("HVV1312", "surfaceTextureListener onSurfaceTextureSizeChanged $p1 - $p2")
+        }
+
+        override fun onSurfaceTextureDestroyed(p0: SurfaceTexture?): Boolean {
+            Log.e("HVV1312", "surfaceTextureListener onSurfaceTextureDestroyed")
+            return false
+        }
+
+        override fun onSurfaceTextureUpdated(p0: SurfaceTexture?) {
+            Log.e("HVV1312", "surfaceTextureListener onSurfaceTextureUpdated")
+        }
+
     }
 
 }
